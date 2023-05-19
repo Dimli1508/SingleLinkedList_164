4d331a4a2ddc2328ea754c1d2047916d808a828f
Mashell0707 <merlsutama@gmail.com>
2023-05-19 13:04:04 +0700

    case 5

--- SingleLinkedList_164/SingleLinkedList_164.cpp
+++ SingleLinkedList_164/SingleLinkedList_164.cpp
@@ -188,4 +188,17 @@ int main() {
 			case 4:
 				searchData();
 				break;
-			
+			case 5:
+				break;
+			default:
+				cout << "pilihan tidak ada" << endl;
+				break;
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Terjadi Kesalahan" << endl;
+		}
+	} while (pilihan != 5);
+}
+
